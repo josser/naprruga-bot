6444c028d1c2a60887278ebbfeaa258db3291766
Dmytro Chyrkin <dmitry.chirkin@gmail.com>
2026-03-24 23:00:48 +0200

    fix: migrated to better-sqlite

--- src/knexfile.cjs
+++ src/knexfile.cjs
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-   client: "sqlite3",
+   client: "better-sqlite3",
    useNullAsDefault: true,
    connection: {
       filename: path.resolve(__dirname, '..', 'db.sqlite'),
